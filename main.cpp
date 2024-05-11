f7516bcada90c1598565b11188edb620078839c2
Nikita Krenov <nikitabaram@gmail.com>
2024-05-11 15:37:37 +0200

    The program guesses a number in the range from 1 to 500. The user tries to guess it. After each attempt, the program gives hints whether the number is greater or less than the guessed number. At the end, the program gives statistics: in how many attempts the number was guessed, how long it took. Predict an output of 0 if the user is tired of guessing the number.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,47 @@
-#include <iostream>;
-#include <string>;
+#include <iostream>
+#include <string>
+#include <ctime>
 using namespace std;
 
 int main() {
 
+    srand(time(0));
+    int guessNumber = rand() % 500 + 1;
+    int guess;
+    int attempts = 0;
+    clock_t start, end;
+
+    cout << "Guess the number between 1 and 500. Enter 0 to exit: ";
+
+    start = clock();
+
+    for (;;) {
+        cout << "Your guess: ";
+        cin >> guess;
+        attempts++;
+
+        if (guess == 0) {
+            cout << "Game over. Goodbye!";
+            break;
+        }
+        else if (guess == guessNumber) {
+            cout << "Congratulations! The number is " << guessNumber << endl;
+            break;
+        }
+        else if (guess < guessNumber) {
+            cout << "The secret number is greater." << endl;
+        }
+        else {
+            cout << "The secret number is smaller." << endl;
+        }
+    }
+
+    end = clock();
+    double time_taken = double(end - start) / CLOCKS_PER_SEC;
+
+    cout << "Statistics: " << endl;
+    cout << "Attempts: " << attempts << endl;
+    cout << "Time: " << time_taken << " seconds" << endl;
 
 	system("pause");
 	return 0;
