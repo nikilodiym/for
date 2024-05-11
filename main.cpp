f1462013a1343114fc16f72ec4779614de30883e
Nikita Krenov <nikitabaram@gmail.com>
2024-05-11 17:04:34 +0200

    The user enters any integer. It is necessary to remove all digits 3 and 6 from this integer and display it back on the screen.

--- main.cpp
+++ main.cpp
@@ -3,7 +3,21 @@
 using namespace std;
 
 int main() {
+	int num, position, digit, temp, result = 0;
+	cout << "Enter a number: ";
+	cin >> num;
+	temp = num;
 
+	for (position = 1; temp > 0; position *= 10) {
+		digit = temp % 10;
+		temp /= 10;
+
+		if (digit != 3 && digit != 6) {
+			result += digit * position;
+		}
+	}
+
+	cout << "The number without 3 and 6 is: " << result << endl;
 
 	system("pause");
 	return 0;
