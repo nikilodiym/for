df493910fd5308ccd19efa19cbddc5223bc58f79
Nikita Krenov <nikitabaram@gmail.com>
2024-05-11 16:51:01 +0200

    the number of integers in the range 100 to 999 in which all digits are distinct.

--- main.cpp
+++ main.cpp
@@ -3,7 +3,19 @@
 using namespace std;
 
 int main() {
+	int count = 0;
 
+	for (int i = 100; i <= 999; i++) {
+		int a = i / 100;
+		int b = (i / 10) % 10;
+		int c = i % 10;
+
+		if (a != b && b != c && c != a) {
+			count++;
+		}
+
+		cout << "Number of integers with all different digits: " << count << endl;
+	}
 
 	system("pause");
 	return 0;
