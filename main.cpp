50ff76f48d00fe7cdf75ca448ea58385a0b91005
Nikita Krenov <nikitabaram@gmail.com>
2024-05-11 16:43:26 +0200

    the number of integers in the range 100 to 999 that have two identical digits.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,19 @@
 using namespace std;
 
 int main() {
+	int count = 0;
+
+	for (int i = 100; i <= 999; i++) {
+		int a = i / 100;
+		int b = (i / 10) % 100;
+		int c = i % 10;
+
+		if (a == b || b == c || a == c) {
+			count++;
+		}
+	}
+
+	cout << "The number of integers with two identical digits: " << count << endl;
 
 
 	system("pause");
