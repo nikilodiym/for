a26bd81306f8566df53b801a0535fc9926c2b77c
Nikita Krenov <nikitabaram@gmail.com>
2024-05-11 17:09:11 +0200

    The user enters an arbitrary integer A. It is necessary to output all integers B for which A is divisible without a remainder by B*B and is not divisible without a remainder by B*B*B.

--- main.cpp
+++ main.cpp
@@ -3,7 +3,15 @@
 using namespace std;
 
 int main() {
+	int A, B;
+	cout << "Enter a number A: ";
+	cin >> A;
 
+	for (B = 1; B <= A; B++) {
+		if (A % B == 0 && A % (B * B * B) != 0) {
+			cout << B << " " << endl;
+		}
+	}
 
 	system("pause");
 	return 0;
