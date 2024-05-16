a6bb93dcd93b9857f6c035d5c6c1173f9fb1ead4
Nikita Krenov <nikitabaram@gmail.com>
2024-05-16 12:13:17 +0200

    enters two integers. It is necessary to output all integers by which both entered numbers are divisible without a remainder.

--- main.cpp
+++ main.cpp
@@ -3,8 +3,20 @@
 using namespace std;
 
 int main() {
+	int num1, num2;
+	cout << "Enter the first number: ";
+	cin >> num1;
+	cout << "Enter the second number: ";
+	cin >> num2;
 
-
+	cout << "Common divisors of " << num1 << " and " << num2 << " are: ";
+	int smaller = (num1 < num2) ? num1 : num2;
+	for (int i = 1; i <= smaller; ++i) {
+		if (num1 % i == 0 && num2 % i == 0) {
+			std::cout << i << " ";
+		}
+	}
+	cout << endl;
 	system("pause");
 	return 0;
 }
