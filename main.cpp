7024d7bfad56bd2593f3d508e16d33b882451b91
Nikita Krenov <nikitabaram@gmail.com>
2024-05-16 11:44:53 +0200

    The user enters an integer. It is necessary to output all integers by which the given number is divided without a remainder.

--- main.cpp
+++ main.cpp
@@ -3,7 +3,18 @@
 using namespace std;
 
 int main() {
+	int number;
+	cout << "Enter a number: ";
+	cin >> number;
 
+	cout << "The number" << number << "is divisible by: " << endl;
+	for (int i = 1; i <= number; ++i) {
+        if (number % i == 0) {
+            std::cout << i << " ";
+        }
+    }
+
+	cout << endl;
 
 	system("pause");
 	return 0;
